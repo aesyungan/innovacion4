e98d251026fa2844af5fd92eeaf707a24bb7d92c
XL <XL@XL>
2017-02-18 15:52:36 -0500

    crud completo categoria

--- services/s_cliente_logear.php
+++ services/s_cliente_logear.php
@@ -1,13 +1,11 @@
 <?php
-
-
  require_once '../controlador/controlador_cliente.php';
   require_once '../modelo/modelo_cliente.php';
 $item= new controlador_cliente();
 $modelo=new modelo_cliente();
-$modelo->cedula=  isset($_GET['nombre'])?$_GET['nombre']:null;
-$modelo->password=isset($_GET['password'])?$_GET['password']:null;
-//$modelo->cedula= "0603697891";
-//$modelo->password="cuak";
+//$modelo->cedula=  isset($_GET['nombre'])?$_GET['nombre']:null;
+//$modelo->password=isset($_GET['password'])?$_GET['password']:null;
+$modelo->cedula= "0603697891";
+$modelo->password="cuak";
 echo $item->Logear($modelo);
 
