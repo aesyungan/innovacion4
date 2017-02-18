093beadf8d116f4b911d65eff8a9023bbf70a8d1
XL <XL@XL>
2017-02-17 23:34:58 -0500

    crud completo categoria

---
+++ modelo/modelo_producto.php
@@ -0,0 +1,10 @@
+<?php
+class modelo_producto {
+    public $id_producto;
+    public $nombre;
+    public $precio;
+    public $stock; 
+    public $id_categoria; 
+    public $format;
+    public $content;
+}
