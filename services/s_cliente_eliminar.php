33f772bc25b3f887d3be7b49e4ca36b2f60f461e
Enrique <Enrique@Edwin>
2017-02-18 12:57:45 -0500

    servicio cliente

--- services/s_cliente_eliminar.php
+++ services/s_cliente_eliminar.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once '../controlador/controlador_cliente.php';
-$item=new controlador_cliente();
+$item=new controlador_cliente;
 $modelo=new modelo_cliente();
 $modelo->id_cliente=  isset($_GET['id_cliente'])?$_GET['id_cliente']:null;
-echo $item->android_eliminar($item);
+echo $item->EliminarClinete($modelo);
