3a5da63e029ab5a65446e124aec9d436d773c3c2
Enrique <Enrique@Edwin>
2017-02-18 12:43:38 -0500

    servicio cliente

--- services/s_cliente_eliminar.php
+++ services/s_cliente_eliminar.php
@@ -4,4 +4,4 @@ require_once '../controlador/controlador_cliente.php';
 $item=new controlador_cliente();
 $modelo=new modelo_cliente();
 $modelo->id_cliente=  isset($_GET['id_cliente'])?$_GET['id_cliente']:null;
-echo $item->elimianr_cliente($modelo);
+echo $item->android_eliminar($item);
