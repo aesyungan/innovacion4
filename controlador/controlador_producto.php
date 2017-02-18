a5b1cfd4fe3deb02d833c8dc9514d4b5400840f2
XL <XL@XL>
2017-02-17 23:57:51 -0500

    crud completo categoria

--- controlador/controlador_producto.php
+++ controlador/controlador_producto.php
@@ -2,7 +2,7 @@
 
 class controlador_producto extends connect {
    public function android_listar() {
-        $consulta = "SELECT * FROM Producto";
+        $consulta = "SELECT p.id_producto,p.nombre,p.precio,p.stock,p.format,p.content,c.id_categoria,c.nombre as categoria_nombre,c.descripcion FROM Producto as p INNER JOIN Categoria as c on c.id_categoria=p.id_categoria";
         $res_consulta = $this->conectar($consulta);
         $res["success"] = 0;
         $res["messages"] = "Correcto...";
@@ -13,9 +13,15 @@ class controlador_producto extends connect {
             $res["productos"] = array();
             while ($dato = mysqli_fetch_array($res_consulta)) {
                 $item = array();
-                $item["id_categoria"] = $dato["id_categoria"];
+                $item["id_producto"] = $dato["id_producto"];
                 $item["nombre"] = $dato["nombre"];
-                $item["descripcion"] = $dato["descripcion"];
+                $item["precio"] = $dato["precio"];
+                $item["stock"] = $dato["stock"];
+                $item["format"] = $dato["format"];
+                $item["content"] = $dato["content"];
+                $item["id_categoria"] = $dato["id_categoria"];
+                $item["categoria_nombre"] = $dato["categoria_nombre"];
+                $item["descripcion "] = $dato["descripcion"];
                 array_push($res["productos"], $item);
             }
         } else {
@@ -36,10 +42,10 @@ class controlador_producto extends connect {
         $res["messages"] = "Correcto..";
         return json_encode($res);
     }
-    public function android_modificar(modelo_categoria $item) {
+    public function android_modificar(modelo_producto $item) {
        
 
-        $consulta = "UPDATE `Categoria` SET `nombre`='$item->nombre',`descripcion`='$item->descripcion' WHERE `id_categoria`=$item->id_categoria";
+        $consulta = "UPDATE `Producto` SET`nombre`='$item->nombre',`precio`='$item->precio',`stock`='$item->stock',`id_categoria`=$item->id_categoria,`format`='$item->format',`content`='$item->content' WHERE id_producto=$item->id_producto";
         $res_consulta = $this->conectar($consulta);
         $res["success"] = 1;
         $res["messages"] = "Correcto..";
