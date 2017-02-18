8f0c2028309cd7df8f386aa738f6b077734e7d73
XL <XL@XL>
2017-02-18 08:55:08 -0500

    crud completo categoria

---
+++ services/s_detalle_update.php
@@ -0,0 +1,11 @@
+<?php
+require_once '../controlador/controlador_detalle.php.php';
+$item=new controlador_detalle();
+$modelo=new modelo_detalle();
+$modelo->num_detalle=isset($_GET['num_detalle'])?$_GET['num_detalle']:null;
+$modelo->num_factura=isset($_GET['num_factura'])?$_GET['num_factura']:null;
+$modelo->id_producto=isset($_GET['id_producto'])?$_GET['id_producto']:null;
+$modelo->catidad=isset($_GET['catidad'])?$_GET['catidad']:null;
+$modelo->precio=isset($_GET['precio'])?$_GET['precio']:null;
+echo $item->android_modificar($modelo);
+
