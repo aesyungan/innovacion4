8f1afd1939ebbb7c91f5bc35597c005538b6463d
Enrique <Enrique@Edwin>
2017-02-18 11:36:33 -0500

    modo_factura

---
+++ modelo/modelo_cliente.php
@@ -0,0 +1,13 @@
+<?php
+class modelo_cliente{
+    public  $id_cliente;
+    public $nombre;
+    public $apellido;
+    public $direccion;
+    public $fecha_nacimiento;
+    public $telefono;
+    public $email;
+    public $foto;
+    
+    
+  }
