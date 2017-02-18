8a78de3fed1724450412a9145d4390f385012c7e
Enrique <Enrique@Edwin>
2017-02-17 23:52:45 -0500

    modo_pago edwin

---
+++ modelo/modelo_modo_pago.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of modelo_modo_pago
+ *
+ * @author Enrique
+ */
+class modelo_modo_pago {
+    public $num_pago;
+    public $nombre;
+    public $otros_detalles;
+}
