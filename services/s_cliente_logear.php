59f8eac98ffcc47e311f81589dde606e6fe8a678
XL <XL@XL>
2017-02-19 00:17:14 -0500

    crud completo categoria

--- services/s_cliente_logear.php
+++ services/s_cliente_logear.php
@@ -3,10 +3,10 @@
   require_once '../modelo/modelo_cliente.php';
 $item= new controlador_cliente();
 $modelo=new modelo_cliente();
-//$modelo->cedula=  isset($_GET['nombre'])?$_GET['nombre']:null;
-//$modelo->password=isset($_GET['password'])?$_GET['password']:null;
+$modelo->cedula=  isset($_GET['cedula'])?$_GET['cedula']:null;
+$modelo->password=isset($_GET['password'])?$_GET['password']:null;
 //cachetes mandarina
-$modelo->cedula= "0603697891";
-$modelo->password="cuak";
+//$modelo->cedula= "0603697891";
+//$modelo->password="cuak";
 echo $item->Logear($modelo);
 
